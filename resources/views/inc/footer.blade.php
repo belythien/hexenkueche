e000d2991f7ff8e8351bd3a9378da315bafa2c33
Sebastian <s.hoewer@gmail.com>
2019-09-07 14:17:52 +0200

    Footer responsive korrigiert

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,28 +1,32 @@
 <footer class="footer bg-dark">
     <div class="container">
-        <span class="text-muted">&copy; Hexenküche {{ now()->year }}</span>
-        <div class="float-right">
-            @if(isset($menus[2]))
-                @foreach($menus[2]->pages as $page)
-                    @if($page->isLive() == 1)
-                        @if(!empty($page->external_url))
-                            <a class="ml-3" href="{{ $page->external_url }}" target="_blank">{{ $page->menu_title }}</a>
-                        @else
-                            <a class="ml-3" href="{{ route('page', [$page->slug]) }}">{{ $page->menu_title }}</a>
-                        @endif
-                    @else
-                        @auth
+        <div class="row">
+            <div class="col-md-3">
+                <span class="text-muted">&copy; Hexenküche {{ now()->year }}</span>
+            </div>
+            <div class="col-md-9 text-right">
+                @if(isset($menus[2]))
+                    @foreach($menus[2]->pages as $page)
+                        @if($page->isLive() == 1)
                             @if(!empty($page->external_url))
-                                <a class="ml-3 footer-link-inactive" href="{{ $page->external_url }}" target="_blank"
-                                >{{ $page->menu_title }}</a>
+                                <a class="ml-3" href="{{ $page->external_url }}" target="_blank">{{ $page->menu_title }}</a>
                             @else
-                                <a class="ml-3 footer-link-inactive" href="{{ route('page', [$page->slug]) }}"
-                                >{{ $page->menu_title }}</a>
+                                <a class="ml-3" href="{{ route('page', [$page->slug]) }}">{{ $page->menu_title }}</a>
                             @endif
-                        @endauth
-                    @endif
-                @endforeach
-            @endif
+                        @else
+                            @auth
+                                @if(!empty($page->external_url))
+                                    <a class="ml-3 footer-link-inactive" href="{{ $page->external_url }}" target="_blank"
+                                    >{{ $page->menu_title }}</a>
+                                @else
+                                    <a class="ml-3 footer-link-inactive" href="{{ route('page', [$page->slug]) }}"
+                                    >{{ $page->menu_title }}</a>
+                                @endif
+                            @endauth
+                        @endif
+                    @endforeach
+                @endif
+            </div>
         </div>
     </div>
 </footer>
