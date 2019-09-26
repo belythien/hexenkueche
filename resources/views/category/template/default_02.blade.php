608f81ea650a37a25435ab3f6c789f824a8ce437
Sebastian <s.hoewer@gmail.com>
2019-09-26 18:37:24 +0200

    Default Templates fuer Categories

---
+++ resources/views/category/template/default_02.blade.php
@@ -0,0 +1,69 @@
+<div class="col-12 specials mb-4">
+    <h2 class="category_name mt-4">
+        @auth
+            <a href="{{ route('category.edit', [ $category->id ]) }}"
+               class="btn btn-success btn-sm mr-1"
+            ><i class="fas fa-edit"></i></a>
+        @endauth
+        {{ $category->name }}
+    </h2>
+    @if(isset($category->description))
+        <p>{!! $category->description !!}</p>
+    @endif
+    <div class="row">
+        @foreach($category->menuItems as $menuItem)
+            @if($menuItem->isLive())
+                <div class="mb-4 col-md-6">
+                    <h3>
+                        @auth
+                            <a href="{{ route('menuitem.edit', [ $menuItem->id ]) }}"
+                               class="btn btn-danger btn-sm mr-1"
+                            ><i class="fas fa-edit"></i></a>
+                        @endauth
+                        {{ $menuItem->name }}@foreach($menuItem->allergens as $allergen)
+                            <sup
+                                class="allergen" title="{{$allergen->name}}"
+                            >{{ $allergen->id }}</sup>@endforeach
+                    </h3>
+                    <p class="menu_item_description">
+                        @if($menuItem->status == 2)
+                            <span class="menu_item_not_available">Zur Zeit leider nicht erhältlich</span>
+                        @endif
+                        {!! $menuItem->description !!}
+                    </p>
+                    @foreach($menuItem->options as $option)
+                        <div class="container">
+                            <div class="row dotted_line">
+                                <div class="col-xl-8 col-lg-6">
+                                    {!! $option->name !!}
+                                </div>
+                                <div class="col-xl-2 col-lg-3 text-right">
+                                    {{ $option->amount }}
+                                </div>
+                                <div class="col-xl-2 col-lg-3 text-right">
+                                    {{ str_replace('.', ',', number_format($option->price, 2)) }}
+                                    €
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            @endif
+        @endforeach
+    </div>
+    <div class="row mb-4">
+        @foreach($category->menuItems as $menuItem)
+            @foreach( $menuItem->images as $image )
+                <div class="col-md-3">
+                    <a href="{{ route('image.show', [$image->id]) }}">
+                        <img
+                            src="{{ asset('storage/img/' . $image->filename ) }}"
+                            alt="" class="img-thumbnail my-2"
+                            title="{{ $image->name  }}"
+                        />
+                    </a>
+                </div>
+            @endforeach
+        @endforeach
+    </div>
+</div>
---
+++ resources/views/category/template/default_03.blade.php
@@ -0,0 +1,67 @@
+<div class="col-12 specials mb-4">
+    <h2 class="category_name mt-4">
+        @auth
+            <a href="{{ route('category.edit', [ $category->id ]) }}"
+               class="btn btn-success btn-sm mr-1"
+            ><i class="fas fa-edit"></i></a>
+        @endauth
+        {{ $category->name }}
+    </h2>
+    @if(isset($category->description))
+        <p>{!! $category->description !!}</p>
+    @endif
+    <div class="row">
+        @foreach($category->menuItems as $menuItem)
+            @if($menuItem->isLive())
+                <div class="mb-4 col-lg-3">
+                    <h3>
+                        @auth
+                            <a href="{{ route('menuitem.edit', [ $menuItem->id ]) }}"
+                               class="btn btn-danger btn-sm mr-1"
+                            ><i class="fas fa-edit"></i></a>
+                        @endauth
+                        {{ $menuItem->name }}@foreach($menuItem->allergens as $allergen)
+                            <sup
+                                class="allergen" title="{{$allergen->name}}"
+                            >{{ $allergen->id }}</sup>@endforeach
+                    </h3>
+                    <p class="menu_item_description">
+                        @if($menuItem->status == 2)
+                            <span class="menu_item_not_available">Zur Zeit leider nicht erhältlich</span>
+                        @endif
+                        {!! $menuItem->description !!}
+                    </p>
+                    @foreach($menuItem->options as $option)
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    {!! $option->name !!}
+                                </div>
+                            </div>
+                            <div class="row dotted_line">
+                                <div class="col-lg-6 text-right">
+                                    {{ $option->amount }}
+                                </div>
+                                <div class="col-lg-6 text-right">
+                                    {{ str_replace('.', ',', number_format($option->price, 2)) }}
+                                    €
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="mb-4 col-lg-3">
+                    @foreach( $menuItem->images as $image )
+                        <a href="{{ route('image.show', [$image->id]) }}">
+                            <img
+                                src="{{ asset('storage/img/' . $image->filename ) }}"
+                                alt="" class="img-thumbnail my-2"
+                                title="{{ $image->name  }}"
+                            />
+                        </a>
+                    @endforeach
+                </div>
+            @endif
+        @endforeach
+    </div>
+</div>
